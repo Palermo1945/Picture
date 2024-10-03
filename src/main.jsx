eaa7b125f0be8f9c9065d2dfd116ca52e6fadc23
thy-christian <thy.christianpalermo@gmail.com>
2024-10-03 17:57:31 +0800

    fixas

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import Video from './components/Video_gen'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Video />
 
   </StrictMode>,
 )
